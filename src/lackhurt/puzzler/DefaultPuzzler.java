f95d8f2bffeda19006adb100a18f50e7b3c89379
lackhurt <january9527@126.com>
2014-03-22 15:11:04 +0800

    修改编码

--- src/lackhurt/puzzler/DefaultPuzzler.java
+++ src/lackhurt/puzzler/DefaultPuzzler.java
@@ -3,10 +3,9 @@ package lackhurt.puzzler;
 import org.apache.log4j.Logger;
 
 public class DefaultPuzzler {
-	private Logger log = Logger.getLogger(DefaultPuzzler.class);
+//	private Logger log = Logger.getLogger(DefaultPuzzler.class);
 	
 	public static void main(String args[]) {
-		new DefaultPuzzler().log.log(null, "123");
-		
+//		new DefaultPuzzler().log.log(null, "123");
 	}
 }
